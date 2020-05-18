c55158f6ab78e80c4f5a66a2f534ae728cdb8819
amit-shahwal <amitshahwal24@gmail.com>
2020-05-18 15:38:45 +0530

    Added list-tuples-dict

--- Basics.py
+++ Basics.py
@@ -1,2 +1,12 @@
 import datetime
-print("The date and time is",datetime.datetime.now())
+# print("The date and time is",datetime.datetime.now())
+x=datetime.datetime.now();
+
+print (f"the date time is {x}")
+# grades=[1,2,3,[1,3]]
+# # length=len(grades)
+# # print(length,sum(grades))
+# print(grades)
+# dicti={ 'name': "amit",'clas': "4thsem"}
+# print(dicti.values())
+# print(dicti.keys())
