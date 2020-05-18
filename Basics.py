22549e8253912b860ed7592cebc4a598adf3dda7
amit-shahwal <amitshahwal24@gmail.com>
2020-05-18 15:54:05 +0530

    `date`

--- Basics.py
+++ Basics.py
@@ -2,7 +2,7 @@ import datetime
 # print("The date and time is",datetime.datetime.now())
 x=datetime.datetime.now();
 message= f"the date time is {x}"
-print ("the date tim)
+print (message)
 # grades=[1,2,3,[1,3]]
 # # length=len(grades)
 # # print(length,sum(grades))
