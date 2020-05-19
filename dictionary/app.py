a4d4dfd83bcfda703bfc0f8aa2ed55d0144757e0
amit-shahwal <amitshahwal24@gmail.com>
2020-05-19 22:17:48 +0530

    dictionary_app1.2:Tue, May 19, 2020 10:17:48 PM

--- dictionary/app.py
+++ dictionary/app.py
@@ -8,6 +8,10 @@ def getmeaning(word):
     word=word.lower()
     if word in data:
         return data[word]
+    elif word.title() in data:
+        return data[word.title()]
+    elif word.upper() in data:
+        return data[word.upper()]
     elif len(get_close_matches(word,data.keys()))>0:
         cs=input(f"'{get_close_matches(word,data.keys())[0]}' intead of {word}? if yes press 'Y' else 'N: ")
         if cs == "y" or cs == 'y':
